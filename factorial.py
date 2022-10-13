a695473086e1176e672c0000dda4d75555538273
PamperedGenius6098 <56474204+PamperedGenius6098@users.noreply.github.com>
2022-10-13 19:34:51 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,9 +1,9 @@
 ch='y'
 while (ch=='y') or (ch=='Y'):
-    a=int(input("enter the no. of which you want to find factorial:"))
+    a=int(input("Enter the number of which you want to find factorial:"))
     pr=1
     for i in range(1,a+1):
         pr=pr*i
     print(pr,"is the factorial of",a)
-    ch=input("do you want to do more:")
-print("goodbye")
+    ch=input("Do you want to do more:")
+print("Goodbye")
